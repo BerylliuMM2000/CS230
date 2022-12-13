5870c33892eea7d42dc1147887e4279e37b82c57
fang2020shu <66793109+fang2020shu@users.noreply.github.com>
2022-12-12 23:42:26 -0800

    Update SemiSupervisedCL-DataGenerator.py

--- ContrastiveLearning/SemiSupervised/SemiSupervisedCL-DataGenerator.py
+++ ContrastiveLearning/SemiSupervised/SemiSupervisedCL-DataGenerator.py
@@ -10,12 +10,12 @@ import numpy as np
 
 from tensorflow.keras.preprocessing.image import ImageDataGenerator
 
-image_size = 256
+image_size = 208
 
-unlabeled_dataset_size = 3276
-labeled_dataset_size = 820
+unlabeled_dataset_size = 4607 # the 9/10 of images of all 4 classes' training set
+labeled_dataset_size = 514 # 
 
-image_channels = 3
+image_channels = 1
 
 # Algorithm hyperparameters
 num_epochs = 50
@@ -27,13 +27,11 @@ contrastive_augmentation = {"min_area": 0.25, "brightness": 0.6, "jitter": 0.2}
 classification_augmentation = {"min_area": 0.75, "brightness": 0.3, "jitter": 0.1}
 
 
+# Simultaneously load a larger batch of unlabelled images along with a smaller batch of labelled imagee
 def prepare_dataset():
     steps_per_epoch = (unlabeled_dataset_size + labeled_dataset_size) // batch_size
     unlabeled_batch_size = unlabeled_dataset_size // steps_per_epoch
     labeled_batch_size = labeled_dataset_size // steps_per_epoch
-    print(
-        f"batch size is {unlabeled_batch_size} (unlabeled) + {labeled_batch_size} (labeled)"
-    )
 
     unlabeled_train_dataset = tf.keras.utils.image_dataset_from_directory("./Try14",labels = "inferred",
         color_mode = "greyscale",batch_size = unlabeled_batch_size,shuffle=True)
@@ -41,11 +39,8 @@ def prepare_dataset():
     labeled_train_dataset = tf.keras.utils.image_dataset_from_directory("./Try15",labels = 'inferred',
         color_mode = "grescale",batch_size = labeled_batch_size,shuffle=True)
 
-    train_dataset = tf.data.Dataset.zip(
-        (unlabeled_train_dataset, labeled_train_dataset)).prefetch(buffer_size=tf.data.AUTOTUNE)
+    train_dataset = tf.data.Dataset.zip((unlabeled_train_dataset, labeled_train_dataset)).prefetch(buffer_size=tf.data.AUTOTUNE)
 
     test_dataset = tf.keras.utils.image_dataset_from_directory("./Try16",labels = 'inferred',batch_size = batch_size,)
     return train_dataset, labeled_train_dataset, test_dataset
     
-   
-train_dataset, labeled_train_dataset, test_dataset = prepare_dataset()
